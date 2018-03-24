4b578e91a42f03b78b853ff87cdc288cd8fd5559
darbs <darby.perez@gmail.com>
2018-03-23 23:42:23 -0500

    learned a little go

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -1,18 +1,12 @@
 package entity
 
-import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-)
-
 type Tweet struct {
 	EntityId  	string  `json:"entity_id"`
 	Creator 	string  `json:"expanded_url"`
 	Content     string  `json:"url"`
 }
 
-
+// todo all this bs
 func Save() {
 
 }
--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -1,5 +1,52 @@
 package fetch
 
 import (
-	"github.com/darbs/thavma-twitter/internal/entity"
+	"fmt"
+	"log"
+	"os"
+
+	//"github.com/darbs/thavma-twitter/internal/entity"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
+
+var (
+	twitterConsumerKey    = os.Getenv("TWITTER_CONSUMER_KEY")
+	twitterConsumerSecret = os.Getenv("TWITTER_CONSUMER_SECRET")
+	twitterAccessToken    = os.Getenv("TWITTER_ACCESS_TOKEN")
+	twitterAccessSecret   = os.Getenv("TWITTER_ACCESS_SECRET")
+	twClient              *twitter.Client
+)
+
+func init() {
+	if twitterConsumerKey == "" || twitterConsumerSecret == "" || twitterAccessToken == "" || twitterAccessSecret == "" {
+		log.Fatal("Consumer key/secret and Access token/secret required")
+	}
+
+	config := oauth1.NewConfig(twitterConsumerKey, twitterConsumerSecret)
+	token := oauth1.NewToken(twitterAccessToken, twitterAccessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+	twClient = twitter.NewClient(httpClient)
+}
+
+func Get(symbol string) {
+	fmt.Printf("Fetching Tweets for %s\n", symbol)
+
+	search, _, err := twClient.Search.Tweets(&twitter.SearchTweetParams{
+		Query: symbol, // todo validate symbol
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i := 0
+	l := len(search.Statuses)
+	for i < l {
+		fmt.Printf("Id: %v U: %v R: %v T: %v\n", search.Statuses[i].ID, search.Statuses[i].User.Name, search.Statuses[i].Retweeted, search.Statuses[i].Text)
+		//fmt.Printf("SEARCH TWEETS:\n%+v\n", search.Statuses[i].ID, search.Statuses[i].Text, search.Statuses[i].RetweetedStatus)
+
+		i++
+	}
+	fmt.Printf("SEARCH METADATA:\n%+v\n", search.Metadata)
+}
